c0e6c2a517af62020124f593fe7d3e7c710974c3
Edwin Sutanudjaja <root@puppypc22930>
2025-11-25 18:52:02 +0100

    validation to rseg

--- Code/2_RandomForest/fun_2_3_apply_optimalRF_validation_rseg.R
+++ Code/2_RandomForest/fun_2_3_apply_optimalRF_validation_rseg.R
@@ -20,9 +20,8 @@ process_mapping <- function(mapping_entry, rseg_discharge_selected, rseg_grdc_no
     print(paste("Processing cell_no_land:", cell_no_land, "grdc_no:", grdc_no))
     prediction_data <- read.csv(prediction_file)
 
-print(names(prediction_data))
-
-piet
+#~ print(names(prediction_data))
+#~ piet
 
     # read validation data
     # validation_data <- read.csv(validation_file)
@@ -31,11 +30,10 @@ piet
 grdc_idx = which(rseg_grdc_no == grdc_no)
 
 # Extract the data for the specific coordinate and time range
-validation_data = cbind(prediction_data, rseg_discharge_selected[, grdc_idx])    
+validation_data = cbind(prediction_data$datetime, rseg_discharge_selected[, grdc_idx])    
 
 
-#~     kge_result <- calculate_kge(prediction_data, validation_data, grdc_no, cell_no_land)
-    kge_result <- 0
+    kge_result <- calculate_kge(prediction_data, validation_data, grdc_no, cell_no_land)
 
     return(kge_result)
   } else {
