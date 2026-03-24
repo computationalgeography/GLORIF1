d5cef0b2a029f6c0e615963dd67e6e84ed73e0c1
root <root@puppypc12763.edwin>
2026-03-24 19:29:39 +0100

    try to plot some time series

--- Code/4_timeseries/plot_timeseries.R
+++ Code/4_timeseries/plot_timeseries.R
@@ -1,5 +1,6 @@
 
 # additional library to read directly from a netcdf file
+library(ggplot2)
 library(ncdf4)
 
 # gsim station code
@@ -66,22 +67,44 @@ merged_table = cbind(merged_table, glorif1_discharge_selected)
 names(merged_table)[4] <- "GLORIF1"
 
 # load the pcrglobwb time series
-# TODO: Use Mike's script to convert pcrglobwb time series 
-pcrglobwb_filename = "" 
+pcrglobwb_nc_filename = "/scratch-shared/edwin/_finalizing_pcrglobwb/datasets_for_plots/pcrglobwb_discharge_30min_monthly.nc"
+pcrglobwb_nc <- nc_open(pcrglobwb_nc_filename)
+pcrglobwb_discharge <- ncvar_get(pcrglobwb_nc, "discharge") 
+pcrglobwb_lat <- ncvar_get(pcrglobwb_nc, "lat")
+pcrglobwb_lon <- ncvar_get(pcrglobwb_nc, "lon")
+pcrglobwb_discharge_selected <- pcrglobwb_discharge[which(pcrglobwb_lon == lon), which(pcrglobwb_lat == lat), ]
+names(merged_table)[5] <- "PCR-GLOBWB"
 
+# load the percentile time series
+# - 2.5%
+percentile_02p5_nc_filename = "/scratch-shared/edwin/_finalizing_percentile_02p5/datasets_for_plots/glorif1_discharge_0p025_30min_monthly.nc"
+percentile_02p5_nc <- nc_open(percentile_02p5_nc_filename)
+percentile_02p5_discharge <- ncvar_get(percentile_02p5_nc, "discharge") 
+percentile_02p5_lat <- ncvar_get(percentile_02p5_nc, "lat")
+percentile_02p5_lon <- ncvar_get(percentile_02p5_nc, "lon")
+percentile_02p5_discharge_selected <- percentile_02p5_discharge[which(percentile_02p5_lon == lon), which(percentile_02p5_lat == lat), ]
+merged_table = cbind(merged_table, percentile_02p5_discharge_selected)
+names(merged_table)[5] <- "percentile_02p5"
+# - 97.5%
+percentile_97p5_nc_filename = "/scratch-shared/edwin/_finalizing_percentile_97p5/datasets_for_plots/glorif1_discharge_0p975_30min_monthly.nc"
+percentile_97p5_nc <- nc_open(percentile_97p5_nc_filename)
+percentile_97p5_discharge <- ncvar_get(percentile_97p5_nc, "discharge") 
+percentile_97p5_lat <- ncvar_get(percentile_97p5_nc, "lat")
+percentile_97p5_lon <- ncvar_get(percentile_97p5_nc, "lon")
+percentile_97p5_discharge_selected <- percentile_97p5_discharge[which(percentile_97p5_lon == lon), which(percentile_97p5_lat == lat), ]
+merged_table = cbind(merged_table, percentile_97p5_discharge_selected)
+names(merged_table)[6] <- "percentile_97p5"
 
 # Plotting the monthly chart !
 ####################################################################################################################################
 #
 # x and y- axis scales:
 y_min = 0
-y_max_obs = max(mergedTable$observation,na.rm=T)
-y_max_sim = max( mergedTable$simulation,na.rm=T)
-y_max = max(y_max_obs, y_max_sim)
+y_max = max(merged_table$percentile_97p5)
 if (y_max > 100) {y_max = ceiling((y_max+75)/100)*100} else {y_max = 100}
 #
-x_min = min(mergedTable$date,na.rm=T) - 365*5
-x_max = max(mergedTable$date,na.rm=T)
+x_min = min(merged_table$date,na.rm=T) - 365*5
+x_max = max(merged_table$date,na.rm=T)
 #
 x_info_text = x_min + 365*0.5
 
@@ -89,14 +112,14 @@ x_info_text = x_min + 365*0.5
 
 outplott <- ggplot()
 outplott <- outplott +
- geom_ribbon(data = mergedTable, mapping = aes(ymin = perc_0p025, ymax = perc_0p975), fill = "grey70") +
- geom_line(data = mergedTable, mapping = aes(x = date, y = observation), color =  "red",  size = 0.90)  +  # measurement (gsim)
- geom_line(data = mergedTable, mapping = aes(x = date, y = observation), color = "green", size = 0.90) +  # measurement (rseg)
- geom_line(data = mergedTable, mapping = aes(x = date, y = simulation ), color = "black", size = 0.25) +  # original pcrglobwb
- geom_line(data = mergedTable, mapping = aes(x = date, y = simulation ), color = "blue",  size = 0.35 ) +  # model results
+ geom_ribbon(data = merged_table, mapping = aes(x = date, ymin = percentile_97p5, ymax = percentile_97p5), fill = "grey70") +
+ geom_line(data = merged_table, mapping = aes(x = date, y = GSIM), color =  "red",  size = 0.90)  +  # measurement (gsim)
+ geom_line(data = merged_table, mapping = aes(x = date, y = RSEG), color = "green", size = 0.90)  +  # measurement (rseg)
+ geom_line(data = merged_table, mapping = aes(x = date, y = PCR-GLOBWB ), color = "black", size = 0.25)  +  # original pcrglobwb
+ geom_line(data = merged_table, mapping = aes(x = date, y = GLORIF1 ), color = "blue",  size = 0.35 ) +  # model results
 
-#~  geom_line(data = mergedTable, mapping = aes(x = date, y = observation), color =  "red") + # measurement
-#~  geom_line(data = mergedTable, mapping = aes(x = date, y = simulation ), color = "blue") + # model results
+#~  geom_line(data = merged_table, mapping = aes(x = date, y = observation), color =  "red") + # measurement
+#~  geom_line(data = merged_table, mapping = aes(x = date, y = simulation ), color = "blue") + # model results
 #
 #~  geom_text(aes(x = x_info_text, y = 1.00*y_max, label = attributeStat[1]), size = 2.5,hjust = 0) +
 #~  geom_text(aes(x = x_info_text, y = 0.95*y_max, label = attributeStat[2]), size = 2.5,hjust = 0) +
@@ -127,6 +150,7 @@ outplott <- outplott +
  scale_x_date('',limits=c(x_min,x_max)) +
  theme(legend.position = "none") 
 #ggsave("screen.pdf", plot = outplott,width=30,height=8.25,units='cm')
+ outputFile = "test"
  ggsave(paste(outputFile,".pdf",sep=""), plot = outplott,width=27,height=7,units='cm')
 #
 rm(outplott)
