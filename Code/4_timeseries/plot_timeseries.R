c28d63fd55d07a876b01d5565ed179c485df6b5e
root <root@puppypc12763.edwin>
2026-03-24 21:38:51 +0100

    try to plot some time series

--- Code/4_timeseries/plot_timeseries.R
+++ Code/4_timeseries/plot_timeseries.R
@@ -122,10 +122,10 @@ outplott <- outplott +
 #~  geom_line(data = merged_table, mapping = aes(x = date, y = GLORIF1 ), color = "blue",  size = 0.35 ) +  # model results
 
  geom_ribbon(data = merged_table, mapping = aes(x = date, ymin = percentile_02p5, ymax = percentile_97p5), fill = "grey70") +
- geom_line(data = merged_table, mapping = aes(x = date, y = GSIM), color =  "red", linewidth = 0.90)  +  # measurement (gsim)
- geom_line(data = merged_table, mapping = aes(x = date, y = RSEG), color = "green")  +  # measurement (rseg)
- geom_line(data = merged_table, mapping = aes(x = date, y = PCRGLOBWB ), color = "black")  +  # original pcrglobwb
- geom_line(data = merged_table, mapping = aes(x = date, y = GLORIF1 ), color = "blue") +  # model results
+ geom_line(data = merged_table, mapping = aes(x = date, y = GSIM), color =  "red",   linewidth = 1.00)  +  # measurement (gsim)
+ geom_line(data = merged_table, mapping = aes(x = date, y = RSEG), color = "yellow", linewidth = 0.90)  +  # measurement (rseg)
+ geom_line(data = merged_table, mapping = aes(x = date, y = PCRGLOBWB ), color = "black", linewidth = 0.25)  +  # original pcrglobwb
+ geom_line(data = merged_table, mapping = aes(x = date, y = GLORIF1 ), color = "blue", linewidth = 0.35)) +  # model results
 
 
 #~  geom_line(data = merged_table, mapping = aes(x = date, y = observation), color =  "red") + # measurement
