d90de385292059462009afb25d4e7ea4504fefbb
root <root@puppypc12763.edwin>
2026-03-24 21:51:00 +0100

    try to plot some time series

--- Code/4_timeseries/plot_timeseries.R
+++ Code/4_timeseries/plot_timeseries.R
@@ -9,6 +9,7 @@ gsim_code = "AU_0000107"
 gsim_code = "CN_0000001"
 gsim_code = "ZA_0000008"
 gsim_code = "ZW_0000064"
+gsim_code = "RU_0000141"
 
 # get the coordinates
 gsim_station_table_filename = "/scratch-shared/edwin/_finalizing_glorif1/datasets_for_plots/gsim/preprocess_gsim/station_pixel_mapping_gsim.csv"
@@ -125,8 +126,8 @@ outplott <- outplott +
 #~  geom_line(data = merged_table, mapping = aes(x = date, y = GLORIF1 ), color = "blue",  size = 0.35 ) +  # model results
 
  geom_ribbon(data = merged_table, mapping = aes(x = date, ymin = percentile_02p5, ymax = percentile_97p5), fill = "grey70") +
- geom_line(data = merged_table, mapping = aes(x = date, y = GSIM), color =  "red",   linewidth = 1.25)  +  # measurement (gsim)
- geom_line(data = merged_table, mapping = aes(x = date, y = RSEG), color = "yellow", linewidth = 0.50)  +  # measurement (rseg)
+ geom_line(data = merged_table, mapping = aes(x = date, y = GSIM), color =  "yellow",   linewidth = 2.00)  +  # measurement (gsim)
+ geom_line(data = merged_table, mapping = aes(x = date, y = RSEG), color = "red", linewidth = 1.00)  +  # measurement (rseg)
  geom_line(data = merged_table, mapping = aes(x = date, y = PCRGLOBWB ), color = "black", linewidth = 0.25)  +  # original pcrglobwb
  geom_line(data = merged_table, mapping = aes(x = date, y = GLORIF1 ), color = "blue", linewidth = 0.35) +  # model results
 
