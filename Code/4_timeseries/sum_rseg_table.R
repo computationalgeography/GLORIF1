9a7ad9d7105e08d09c74b5b240be3bd75aabee6c
root <root@puppypc12763.edwin>
2026-04-06 21:01:56 +0200

    create a summary table

---
+++ Code/4_timeseries/sum_rseg_table.R
@@ -0,0 +1,24 @@
+
+#~ edwin@tcn1177.local.snellius.surf.nl:/scratch-shared/edwin/_finalizing_glorif1/rseg_evaluation_with_parallelization_done/summary_table$ ls -lah ../*.txt
+#~ -r--r--r--. 1 edwin edwin 77K Apr  4 21:31 ../_rseg_evaluation_0000001-0000500.txt
+#~ -r--r--r--. 1 edwin edwin 75K Apr  4 21:31 ../_rseg_evaluation_0000501-0001000.txt
+#~ -r--r--r--. 1 edwin edwin 80K Apr  4 21:31 ../_rseg_evaluation_0001001-0001500.txt
+#~ -r--r--r--. 1 edwin edwin 85K Apr  4 21:31 ../_rseg_evaluation_0001501-0002000.txt
+#~ -r--r--r--. 1 edwin edwin 84K Apr  4 21:31 ../_rseg_evaluation_0002001-0002500.txt
+#~ -r--r--r--. 1 edwin edwin 78K Apr  4 21:31 ../_rseg_evaluation_0002501-0003000.txt
+#~ -r--r--r--. 1 edwin edwin 56K Apr  4 21:31 ../_rseg_evaluation_0003001-0003367.txt
+
+table = read.csv("/scratch-shared/edwin/_finalizing_glorif1/rseg_evaluation_with_parallelization_done/_rseg_evaluation_0000001-0000500.txt", header = TRUE, sep = ";")
+full_table <- table
+table = read.csv("/scratch-shared/edwin/_finalizing_glorif1/rseg_evaluation_with_parallelization_done/_rseg_evaluation_0000501-0001000.txt", header = TRUE, sep = ";")
+full_table <- rbind(full_table, table)
+table = read.csv("/scratch-shared/edwin/_finalizing_glorif1/rseg_evaluation_with_parallelization_done/_rseg_evaluation_0001001-0001500.txt", header = TRUE, sep = ";")
+full_table <- rbind(full_table, table)
+table = read.csv("/scratch-shared/edwin/_finalizing_glorif1/rseg_evaluation_with_parallelization_done/_rseg_evaluation_0001501-0002000.txt", header = TRUE, sep = ";")
+full_table <- rbind(full_table, table)
+table = read.csv("/scratch-shared/edwin/_finalizing_glorif1/rseg_evaluation_with_parallelization_done/_rseg_evaluation_0002001-0002500.txt", header = TRUE, sep = ";")
+full_table <- rbind(full_table, table)
+table = read.csv("/scratch-shared/edwin/_finalizing_glorif1/rseg_evaluation_with_parallelization_done/_rseg_evaluation_0002501-0003000.txt", header = TRUE, sep = ";")
+full_table <- rbind(full_table, table)
+table = read.csv("/scratch-shared/edwin/_finalizing_glorif1/rseg_evaluation_with_parallelization_done/_rseg_evaluation_0003001-0003367.txt", header = TRUE, sep = ";")
+full_table <- rbind(full_table, table)
